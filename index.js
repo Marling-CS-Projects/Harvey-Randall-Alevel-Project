b78dbbf90103f579283e6578c12a6634505b814d
Year 12 <year12@RANDWICKs-MacBook-Air.local>
2021-04-23 12:26:24 +0100

    Fixed index.js

--- index.js
+++ index.js
@@ -9,39 +9,6 @@ const handle = apps.getRequestHandler()
 
 apps.prepare()
     .then(async () => {
-        let app = express()
-        let dbo = db.db("Training-Logs");
-
-        setInterval(() => {
-            require('./server/striker.js')(dbo, client)
-        }, 3600000)
-
-
-        app.use(session({
-            secret: 'youshallnotpass',
-            store: store,
-            resave: false,
-            saveUninitialized: false,
-            cookie: {
-                secure: false,
-                maxAge: 31540000000
-            }
-        }));
-        app.use(passport.initialize());
-        app.use(passport.session());
-        app.use(helmet());
-
-        require('./server/basicData.js')(app, passport)
-        require('./server/imageupload.js')(app, upload, client, Discord, dbo)
-
-        app.get("/api/givePeopleGUI", function (req, res) {
-            res.json(true)
-        });
-
-        app.get("/api/giveGUIToMarineGroup", function (req, res) {
-            res.json(true)
-        });
-
 
         app.get('*', function (req, res) {
             if (!(req.url.includes('api') || req.url.includes('xml') || req.url.endsWith("login") || req.url.endsWith("logout"))) {
