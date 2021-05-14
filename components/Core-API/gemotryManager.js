b1450f23072f885afb11789b4a4d547d5f065801
Year 12 <year12@RANDWICKs-MacBook-Air-2.local>
2021-05-14 14:36:02 +0100

    another set of fixes

--- components/Core-API/gemotryManager.js
+++ components/Core-API/gemotryManager.js
@@ -1,25 +1,25 @@
 // Written By Harvey Randall \\
 import { Mesh } from "three";
-import * as three from 'three'
+import * as three from "three";
 let GameCoreShadows = true; // TODO add to GUI
 
 /*eslint complexity: ["error", 20]*/
 export function createGeo(type, data, pos, rot, mat, shadow) {
-  //Create type, using destructuring to pass in values
-  let geo = new three[`${type}`](...data);
+    //Create type, using destructuring to pass in values
+    let geo = new three[`${type}`](...data);
 
-  //Set Geo Position and Rotation
-  geo.position.set(pos);
-  geo.rotation.set(rot);
+    //Set Geo Position and Rotation
+    geo.position.set(pos);
+    geo.rotation.set(rot);
 
-  //Create the Mesh
-  /*eslint new-cap: "error"*/
-  let mesh = Mesh(geo, mat);
+    //Create the Mesh
+    /*eslint new-cap: "error"*/
+    let mesh = Mesh(geo, mat);
 
-  //Handle Reciving shadows and casting
-  mesh.castShadow = shadow && GameCoreShadows ? true : false;
-  mesh.receiveShadow = shadow && GameCoreShadows ? true : false;
+    //Handle Reciving shadows and casting
+    mesh.castShadow = shadow && GameCoreShadows ? true : false;
+    mesh.receiveShadow = shadow && GameCoreShadows ? true : false;
 
-  //Return Mesh
-  return mesh;
+    //Return Mesh
+    return mesh;
 }
