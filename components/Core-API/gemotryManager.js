32a6bf4af88f9a56b134fd065d27ca7b0da67791
Year 12 <year12@RANDWICKs-MacBook-Air-2.local>
2021-05-14 14:20:39 +0100

    Fixed CSS styleing

--- components/Core-API/gemotryManager.js
+++ components/Core-API/gemotryManager.js
@@ -1,18 +1,18 @@
 // Written By Harvey Randall \\
-import * as THREE from "three";
+import * as three from "three";
 let GameCoreShadows = true; // TODO add to GUI
 
 /*eslint complexity: ["error", 20]*/
 export function createGeo(type, data, pos, rot, mat, shadow) {
   //Create type, using destructuring to pass in values
-  let geo = new THREE[`${type}`](...data);
+  let geo = new three[`${type}`](...data);
 
   //Set Geo Position and Rotation
   geo.position.set(pos);
   geo.rotation.set(rot);
 
   //Create the Mesh
-  let mesh = THREE.Mesh(geo, mat);
+  let mesh = three.Mesh(geo, mat);
 
   //Handle Reciving shadows and casting
   mesh.castShadow = shadow && GameCoreShadows ? true : false;
